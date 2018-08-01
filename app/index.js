fd4e61ac4f136019c9414c66583b5de48d9e38b0
Mike Starr <mstarr@rsui.com>
2018-08-01 16:18:39 -0400

    added rimraf

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ var ReactDOM = require('react-dom');
 import 'babel-polyfill';
 var App = require('./components/App');
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.min.css';
+//import 'font-awesome/css/font-awesome.min.css';
 
 
 require('./index.css');
