ad0cccf09fc6b10d31d1c57f9147a350a4bd22ea
Mike Starr <mstarr@rsui.com>
2018-08-09 08:47:57 -0400

    types for crypto

--- app/util/crypto.ts
+++ app/util/crypto.ts
@@ -1,11 +1,17 @@
-const crypto = window.crypto ||
-  window['msCrypto'] || {
-    getRandomValues: array => {
+declare global { 
+  interface Window {
+     msCrypto: Crypto;
+  }
+}
+const crypto: Crypto = window.crypto ||
+  window.msCrypto || {
+    getRandomValues: (array: Uint8Array): Uint8Array => {
       for (let i = 0, l = array.length; i < l; i++) {
         array[i] = Math.floor(Math.random() * 256);
       }
       return array;
-    }
+    },
+    subtle: null  //to make compatible with Crypto interface
   };
 
 // dec2hex :: Integer -> String
@@ -18,7 +24,7 @@ const CryptoUtils = {
 
     // generateId :: Integer -> String
     generateId(len): string {
-        var arr = new Uint8Array((len || 40) / 2)
+        let arr: Uint8Array = new Uint8Array((len || 40) / 2)
         crypto.getRandomValues(arr)
         return Array.from(arr, dec2hex).join('')
     }
