10c451bb12845f44bfb769f9874d4f0c60781453
Mike Starr <mstarr@rsui.com>
2018-08-20 14:45:47 -0400

    fixed linting issues

--- app/components/SearchService.ts
+++ app/components/SearchService.ts
@@ -1,50 +1,44 @@
-import { Observable, of, from, Subscription } from 'rxjs';
-import { Subject } from 'rxjs';
 import log from 'loglevel';
- 
- 
-import { map, filter, catchError, switchMap, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { from, Observable, of, Subject } from 'rxjs';
+import { catchError, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 declare global {
-    interface Window { ctx: any; }
+    interface Window { ctx: any; } // tslint:disable-line
 }
 
 export default class SearchService {
 
-  private searchTerm: Subject<any>; 
+  private searchTerm: Subject<any>;
 
   constructor() {
     this.searchTerm = new Subject<any>();
   }
 
- 
-
-
-  search(term:any) {
-    log.debug(`svc.search:${term}`)
+  public search(term: any) {
+    log.debug(`svc.search:${term}`);
     this.searchTerm.next(term);
   }
 
-  doSearch(term:any): Observable<any> {
-    log.debug(`doSearch:${term}`)
-    let promise = fetch(`https://www.reddit.com/search.json?q=${term}`)
-                  .then(response => response.json())
-                  .then(json => {
-                      return json.data.children
+  public doSearch(term: any): Observable<any> {
+    log.debug(`doSearch:${term}`);
+    const promise = fetch(`https://www.reddit.com/search.json?q=${term}`)
+                  .then((response) => response.json())
+                  .then((json) => {
+                      return json.data.children;
                   });
 
-    return from(promise)
+    return from(promise);
   }
 
-  getResultSubscription(): Observable<any> {
+  public getResultSubscription(): Observable<any> {
     return this.searchTerm.pipe(
                debounceTime(500),
                distinctUntilChanged(),
-               switchMap(term => term ? this.doSearch(term) : of([])),
-               catchError(error => {
-                 console.error(error);
+               switchMap((term) => term ? this.doSearch(term) : of([])),
+               catchError((error) => {
+                 log.error(error);
                  return of([]);
-               })
+               }),
             );
   }
 }
