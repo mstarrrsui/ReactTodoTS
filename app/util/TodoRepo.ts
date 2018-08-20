238fc21f88965b0e46cf20d2971f037988392dc4
Mike Starr <mstarr@rsui.com>
2018-08-20 15:59:02 -0400

    fixed linting issues with TodoList

--- app/util/TodoRepo.ts
+++ app/util/TodoRepo.ts
@@ -1,18 +1,18 @@
 
-import { ITask } from '../models/ITask'
+import { ITask } from '../models/ITask';
 
 const delay: number = 3000;
 
 export default class TodoRepo {
 
-    static loadTasks(): Promise<ITask[]> {
+   public static loadTasks(): Promise<ITask[]> {
 
-        const itemsJson: string = localStorage.getItem('todoitems')
-        let itemsFromStorage: ITask[] = (itemsJson ? JSON.parse(itemsJson) : []) as ITask[]
+        const itemsJson: string = localStorage.getItem('todoitems');
+        const itemsFromStorage: ITask[] = (itemsJson ? JSON.parse(itemsJson) : []) as ITask[];
         return new Promise((resolve, reject) => {
             setTimeout(() => {
               resolve(itemsFromStorage);
             }, delay);
-          })
+          });
     }
 }
