ffa931d09e81ebef76da52e49ecebdabff97ef30
Starr <mstarr@rsui.com>
2018-08-28 16:34:24 -0400

    added capability to run SSL from dist.  added polyfills as entry

--- servedist.js
+++ servedist.js
@@ -9,7 +9,10 @@ var compress = require('compression');
 //var errorHandler = require('./routes/utils/errorHandler')();
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var https = require('https');
+var fs = require('fs');
 var port = process.env.PORT || 7203;
+var sslport = process.env.SSLPORT || 8443;
 
 var environment = process.env.NODE_ENV;
 
@@ -25,21 +28,32 @@ app.use(logger('combined'));
 
 //routes = require('./routes/index')(app);
 
+var sslOptions = {
+    key: fs.readFileSync('ssl/key.pem'),
+    cert: fs.readFileSync('ssl/cert.pem'),
+    passphrase: 'summer'
+  };
+
 console.log('Cranking up express....');
-console.log('PORT=' + port);
-console.log('NODE_ENV=' + environment);
+//console.log('PORT=' + port);
+//console.log('SSL PORT=' + sslport);
+//console.log('NODE_ENV=' + environment);
 
 app.get('/ping', function(req, res, next) {
     console.log(req.body);
     res.send('pong');
 });
 
-console.log('** MEMBER MANAGER - DIST VERSION **');
+console.log('** REACT SAMPLE  TODO APP - DIST VERSION **');
 app.use(express.static('./dist/'));
 app.use('/*', express.static('./dist/index.html'));
 
+//serve ssl
+https.createServer(sslOptions, app).listen(sslport);
+
 app.listen(port, function() {
     console.log('Express server listening on port ' + port);
+    console.log(' HTTPS port ' + sslport);
     console.log('env = ' + app.get('env') +
                 '\n__dirname = ' + __dirname +
                 '\nprocess.cwd = ' + process.cwd());
