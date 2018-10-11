624df31e93eb62df74cdb0c13ac2a5793b573457
Mike Starr <mstarr@rsui.com>
2018-10-11 11:17:31 -0400

    got development config working

---
+++ build-util/webpack.development.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const webpack = require('webpack');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+
+
+module.exports = () => {
+
+  return {
+    devtool: "eval-source-map",
+    module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            loader: "ts-loader",
+            exclude: /node_modules/,
+            options: {
+              //
+              // use transpileOnly mode to speed-up compilation; type checking is done in
+              // a separate process using the ForkTsCheckerWebpackPlugin
+              //
+              transpileOnly: true
+            }
+          },
+          {
+            test: /\.(sa|sc|c)ss$/,
+            use: [
+              'style-loader',
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 1,
+                }
+              },
+              {
+                loader: 'postcss-loader'
+              },
+              {
+                loader: 'sass-loader' //sass loader
+              }
+            ]
+          }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new ForkTsCheckerWebpackPlugin()
+    ],
+    devServer: {
+        quiet: false,
+        noInfo: false,
+        historyApiFallback: true,
+        hot: true,
+        stats: {
+            // Config for minimal console.log mess.
+            assets: true,
+            colors: true,
+            version: true,
+            hash: true,
+            timings: true,
+            chunks: false,
+            chunkModules: false
+          }
+    }
+  }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -2,83 +2,56 @@
 
 const path = require('path');
 const webpack = require('webpack');
+const webpackMerge = require("webpack-merge");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const {InjectManifest} = require('workbox-webpack-plugin');
-const devMode = process.env.NODE_ENV !== 'production'
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const envConfig = mode => require(`./build-util/webpack.${mode}.js`)();
 
-const noop = () => {}
+//common config goes in here
 
-const config = {
-    mode: process.env.NODE_ENV === 'production' ? "production" : "development",
-    devtool: process.env.NODE_ENV === 'production' ? "" : "eval-source-map",
-    entry: {
-      polyfills: './app/polyfills.ts',
-      main: './app/index.tsx'
-    },
-    resolve: {
-      extensions: [".ts", ".tsx", ".js", ".jsx"]
-  },
-    optimization: {
-        minimize: process.env.NODE_ENV === 'production' ? true : false,
-        splitChunks: {
-          cacheGroups: {
-            commons: {
-              chunks: "initial",
-              minChunks: 2
-            },
-            vendor: {
-              test: /node_modules/,
-              chunks: "initial",
-              name: "vendor",
-              priority: 10,
-              enforce: true
+module.exports = ( {mode, visualize=false} = { mode: "development" }) => {
+
+  console.log(`mode: ${mode}`);
+  console.log(`visualize: ${visualize}`);
+
+  let config = webpackMerge(
+    {
+      mode: mode,
+      entry: {
+        polyfills: './app/polyfills.ts',
+        main: './app/index.tsx'
+      },
+      resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"]
+      },
+      optimization: {
+          minimize: false,
+          splitChunks: {
+            cacheGroups: {
+              commons: {
+                chunks: "initial",
+                minChunks: 2
+              },
+              vendor: {
+                test: /node_modules/,
+                chunks: "initial",
+                name: "vendor",
+                priority: 10,
+                enforce: true
+              }
             }
           }
-		    }
-    },
-    performance: {
-        hints: false
-    },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: devMode ? '[name].js' : '[name].[hash].js',
-        publicPath: ''
-    },
-    module: {
+      },
+      performance: {
+          hints: false
+      },
+      output: {
+          path: path.resolve(__dirname, "dist"),
+          filename: '[name].[hash].js',
+          publicPath: ''
+      },
+      module: {
         rules: [
-          {
-            test: /\.tsx?$/,
-            loader: "ts-loader",
-            exclude: /node_modules/,
-            options: {
-              //
-              // use transpileOnly mode to speed-up compilation; type checking is done in
-              // a separate process using the ForkTsCheckerWebpackPlugin
-              //
-              transpileOnly: true
-            }
-          },
-          {
-            test: /\.(sa|sc|c)ss$/,
-            use: [
-              devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-              {
-                loader: 'css-loader',
-                options: {
-                  importLoaders: 1,
-                }
-              },
-              {
-                loader: 'postcss-loader'
-              },
-              {
-                loader: 'sass-loader' //sass loader
-              }
-            ]
-          },
           {
             test: /bootstrap\/dist\/js\/umd\//, use: 'imports-loader?jQuery=jquery'
           },
@@ -101,66 +74,34 @@ const config = {
               }
           }
         ]
-    },
-    plugins: [
-        //new webpack.DefinePlugin({
-        //  BASENAME: devMode ? JSON.stringify("/") : JSON.stringify("/TestWeb1")
-        //}),
-        new HtmlWebpackPlugin({
-          template: 'app/index.html'
-        }),
-        new InjectManifest({
-          swSrc: 'app/mySW.js',
-          swDest: 'service-worker.js'
-        }),
+      },
+      plugins: [
+        new webpack.ProgressPlugin(),
+        new HtmlWebpackPlugin(),
         new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery',
-            Alert: 'exports-loader?Alert!bootstrap/js/dist/alert',
-            Button: 'exports-loader?Button!bootstrap/js/dist/button',
-            Carousel: 'exports-loader?Carousel!bootstrap/js/dist/carousel',
-            Collapse: 'exports-loader?Collapse!bootstrap/js/dist/collapse',
-            Dropdown: 'exports-loader?Dropdown!bootstrap/js/dist/dropdown',
-            Modal: 'exports-loader?Modal!bootstrap/js/dist/modal',
-            Popover: 'exports-loader?Popover!bootstrap/js/dist/popover',
-            Scrollspy: 'exports-loader?Scrollspy!bootstrap/js/dist/scrollspy',
-            Tab: 'exports-loader?Tab!bootstrap/js/dist/tab',
-            Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
-            Util: 'exports-loader?Util!bootstrap/js/dist/util'
-        }),
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
-            filename: devMode ? '[name].css' : '[name].[hash].css',
-            chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
-        }),
-        devMode ? new webpack.HotModuleReplacementPlugin() : noop,
-        devMode ? new ForkTsCheckerWebpackPlugin() : noop
-    ],
-    devServer: {
-        quiet: false,
-        noInfo: false,
-        historyApiFallback: true,
-        hot: true,
-        stats: {
-            // Config for minimal console.log mess.
-            assets: true,
-            colors: true,
-            version: true,
-            hash: true,
-            timings: true,
-            chunks: false,
-            chunkModules: false
-          }
-    }
+          $: 'jquery',
+          jQuery: 'jquery',
+          'window.jQuery': 'jquery',
+          Alert: 'exports-loader?Alert!bootstrap/js/dist/alert',
+          Button: 'exports-loader?Button!bootstrap/js/dist/button',
+          Carousel: 'exports-loader?Carousel!bootstrap/js/dist/carousel',
+          Collapse: 'exports-loader?Collapse!bootstrap/js/dist/collapse',
+          Dropdown: 'exports-loader?Dropdown!bootstrap/js/dist/dropdown',
+          Modal: 'exports-loader?Modal!bootstrap/js/dist/modal',
+          Popover: 'exports-loader?Popover!bootstrap/js/dist/popover',
+          Scrollspy: 'exports-loader?Scrollspy!bootstrap/js/dist/scrollspy',
+          Tab: 'exports-loader?Tab!bootstrap/js/dist/tab',
+          Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
+          Util: 'exports-loader?Util!bootstrap/js/dist/util'
+        })
+      ]
+    },
+    envConfig(mode)
+  );
+
+  if (mode==="production" && visualize) {
+    config.plugins.push(new BundleAnalyzerPlugin());
+  }
+
+  return config;
 };
-
-
-if (process.env.NODE_ENV === 'visualize') {
-    config.plugins.push(
-        new BundleAnalyzerPlugin()
-    );
-}
-
-module.exports = config;
