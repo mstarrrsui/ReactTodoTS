7d4434a9edf7de6a0c9c63df21e6a98fa46bddb0
Mike Starr <mstarr@rsui.com>
2018-10-11 20:07:51 -0400

    formatting

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
 
 export default class Home extends React.Component {
-    public render() {
-        return (
-            <div>
-                <h1>Welcome to the React Starter</h1>
-                <div className="mainimgcontainer">
-                    <img className="mainimg" src="https://upload.wikimedia.org/wikipedia/en/thumb/5/53/Arsenal_FC.svg/1200px-Arsenal_FC.svg.png"/>
-                </div>
-            </div>
-
-        );
-    }
+  public render() {
+    return (
+      <div>
+        <h1>Welcome to the React Starter</h1>
+        <div className="mainimgcontainer">
+          <img
+            className="mainimg"
+            src="https://upload.wikimedia.org/wikipedia/en/thumb/5/53/Arsenal_FC.svg/1200px-Arsenal_FC.svg.png"
+          />
+        </div>
+      </div>
+    );
+  }
 }
--- app/components/TodoForm.tsx
+++ app/components/TodoForm.tsx
@@ -3,74 +3,68 @@ import * as React from 'react';
 import log from 'loglevel';
 
 interface ITodoFormProps {
-    onSubmit: (newTask: string) => void;
-    onClear: () => void;
+  onSubmit: (newTask: string) => void;
+  onClear: () => void;
 }
 
 interface ITodoFormState {
-    todoTask: string;
+  todoTask: string;
 }
 
 const initialState: ITodoFormState = {
-    todoTask: '',
+  todoTask: ''
 };
 
 export default class TodoForm extends React.Component<ITodoFormProps, ITodoFormState> {
+  public state: Readonly<ITodoFormState> = initialState;
 
-    public state: Readonly<ITodoFormState> = initialState;
+  public handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const value = event.target.value;
+    this.setState(() => ({ todoTask: value }));
+  }
 
-    public handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-        const value = event.target.value;
-        this.setState(() => ({ todoTask: value }));
+  public handleClear = () => {
+    const { onClear } = this.props;
+    log.debug('handleClear');
+    onClear();
+  }
+
+  public handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    const { onSubmit } = this.props;
+    log.debug('handleSubmit');
+    event.preventDefault();
+    const newtask = this.state.todoTask;
+    if (newtask.trim().length > 0) {
+      onSubmit(newtask);
+      this.setState(() => ({ todoTask: '' }));
     }
+  }
 
-    public handleClear = () => {
-        const { onClear } = this.props;
-        log.debug('handleClear');
-        onClear();
-    }
-
-    public handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-        const { onSubmit } = this.props;
-        log.debug('handleSubmit');
-        event.preventDefault();
-        const newtask = this.state.todoTask;
-        if (newtask.trim().length > 0) {
-            onSubmit(newtask);
-            this.setState(() => ({ todoTask : '' }));
-        }
-    }
-
-    public render() {
-        const { todoTask } = this.state;
-        return (
-            <form className="form-inline form-row" onSubmit={this.handleSubmit}>
-                <div className="col-md-7 offset-md-1">
-                    <input
-                        type="text"
-                        id="todoTask"
-                        className="form-control-lg w-100"
-                        placeholder="Enter a task"
-                        value={todoTask}
-                        onChange={this.handleChange}
-                    />
-                </div>
-                <div className="col-md-3">
-                    <button
-                        type="submit"
-                        className="btn btn-primary m-2"
-                        disabled={!todoTask}
-                    > Add
-                    </button>
-                    <button
-                        type="button"
-                        onClick={this.handleClear}
-                        className="btn btn-success m-1"
-                    > Clear Completed
-                    </button>
-                </div>
-            </form>
-        );
-    }
-
+  public render() {
+    const { todoTask } = this.state;
+    return (
+      <form className="form-inline form-row" onSubmit={this.handleSubmit}>
+        <div className="col-md-7 offset-md-1">
+          <input
+            type="text"
+            id="todoTask"
+            className="form-control-lg w-100"
+            placeholder="Enter a task"
+            value={todoTask}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className="col-md-3">
+          <button type="submit" className="btn btn-primary m-2" disabled={!todoTask}>
+            {' '}
+            Add
+          </button>
+          <button type="button" onClick={this.handleClear} className="btn btn-success m-1">
+            {' '}
+            Clear Completed
+          </button>
+        </div>
+      </form>
+    );
+  }
 }
