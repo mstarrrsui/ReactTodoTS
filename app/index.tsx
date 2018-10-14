a4a7eae17b2c9fd4defa9a192ceaf3e84d6c49fc
Mike Starr <mstarr@rsui.com>
2018-10-14 09:45:10 -0400

    removed extra bootstrap import

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './components/App';
 
-import 'bootstrap';
 import 'bootstrap/scss/bootstrap.scss'; // tslint:disable-line
 import 'font-awesome/css/font-awesome.min.css'; // tslint:disable-line
 
