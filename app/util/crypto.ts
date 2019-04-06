544b9d7467dedab3def32de7acd07703b31a9928
Mike Starr <mstarr@rsui.com>
2019-04-06 13:07:12 -0400

    last lint cleanup

--- app/util/crypto.ts
+++ app/util/crypto.ts
@@ -1,8 +1,9 @@
 const crypto: Crypto = window.crypto ||
   window.msCrypto || {
-    getRandomValues: (array: any) => {
+    getRandomValues: (array: number[]) => {
+      const newarray: number[] = [];
       for (let i = 0, l = array.length; i < l; i += 1) {
-        array[i] = Math.floor(Math.random() * 256);
+        newarray[i] = Math.floor(Math.random() * 256);
       }
       return array;
     },
@@ -16,7 +17,7 @@ function dec2hex(dec: number): string {
 }
 
 // generateId :: Integer -> String
-export function generateId(len: number): string {
+export default function generateId(len: number): string {
   const arr: Uint8Array = new Uint8Array((len || 40) / 2);
   crypto.getRandomValues(arr);
   return Array.from(arr, dec2hex).join('');
