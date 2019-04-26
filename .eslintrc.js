c8e6de79037b43a44edaac6c76a4dada6ccf62cb
Mike Starr <mstarr@rsui.com>
2019-04-26 16:24:26 -0400

    updates to lint for explicit any

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: [],
+  extends: ['react-app', 'prettier', 'prettier/@typescript-eslint', 'prettier/react'],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true
+    },
+    project: './tsconfig.json'
+  },
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    'linebreak-style': 0,
+    'lines-between-class-members': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  }
+};
--- app/util/typeUtil.ts
+++ app/util/typeUtil.ts
@@ -1,11 +1,13 @@
-import { ReactNode } from 'react';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export type HasRenderProp<T> = T extends { render: any } ? T : never;
+export type HasChildrenProp<T> = T extends { children: any } ? T : never;
+export type IsFunction<T> = T extends (...args: any[]) => any ? T : never;
 
-type HasRenderProp<T> = T extends { render: (props: any) => ReactNode } ? T : never;
-type HasChildrenProp<T> = T extends { children: (props: any) => ReactNode } ? T : never;
-type IsFunction<T> = T extends (...args: any[]) => any ? T : never;
+export const isFunction = <T extends {}>(value: T): value is IsFunction<T> =>
+  typeof value === 'function';
 
-export const isFunction = <T extends {}>(value: T): value is IsFunction<T> => typeof value === 'function';
+export const hasRender = <T extends {}>(value: T): value is HasRenderProp<T> =>
+  'render' in value && isFunction((value as HasRenderProp<T>).render);
 
-export const hasRender = <T extends {}>(value: T): value is HasRenderProp<T> => 'render' in value && isFunction((value as HasRenderProp<T>).render);
-
-export const hasChildren = <T extends {}>(value: T): value is HasChildrenProp<T> => 'children' in value && isFunction((value as HasChildrenProp<T>).children);
+export const hasChildren = <T extends {}>(value: T): value is HasChildrenProp<T> =>
+  'children' in value && isFunction((value as HasChildrenProp<T>).children);
