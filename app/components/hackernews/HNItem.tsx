fc6478397f2120c1abeb0ff80057808456838396
Mike Starr <mstarr@rsui.com>
2019-08-10 15:33:04 -0400

    updates

--- app/components/hackernews/HNItem.tsx
+++ app/components/hackernews/HNItem.tsx
@@ -1,8 +1,5 @@
 export default interface HNItem {
-  story_title: string;
-  story_url: string;
   comment_text: string;
-  story_id: string;
   title: string;
   author: string;
   created_at: string;
