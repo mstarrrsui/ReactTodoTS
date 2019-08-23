edc5ec3999529ee8183fe6a61cec9298f632665e
Mike Starr <mstarr@rsui.com>
2019-08-23 18:38:18 -0400

    create custom hook for hnsearch

--- app/components/hackernews/ResultsList.tsx
+++ app/components/hackernews/ResultsList.tsx
@@ -1,36 +1,9 @@
 import * as React from 'react';
-import { HNItem, mapFromJSON } from './HNItem';
 import ItemRow from './ItemRow';
-import { useState, useEffect } from 'react';
-
-const DATA_URL = 'https://hn.algolia.com/api/v1/search_by_date?page=0&tags=story&hitsPerPage=1000';
+import useHNSearch from './useHNSearch';
 
 const ResultsList: React.SFC = function() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<Error | null>(null);
-  const [items, setItems] = useState<Array<HNItem>>([]);
-
-  useEffect(() => {
-    setIsLoading(true);
-    setError(null);
-    fetch(DATA_URL)
-      .then(res => {
-        if (res.ok) {
-          return res.json();
-        } else {
-          console.log('error...');
-          throw Error('Error fetching the data!!!');
-        }
-      })
-      .then((items: { hits: [] }) => {
-        setIsLoading(false);
-        setItems(items.hits.map(mapFromJSON));
-      })
-      .catch(error => {
-        setIsLoading(false);
-        setError(error);
-      });
-  }, []);
+  const { isLoading, error, items } = useHNSearch();
 
   if (error) {
     return <p>{error.message}</p>;
---
+++ app/components/hackernews/useHNSearch.ts
@@ -0,0 +1,42 @@
+import { HNItem, mapFromJSON } from './HNItem';
+import { useState, useEffect } from 'react';
+
+const DATA_URL = 'https://hn.algolia.com/api/v1/search_by_date?page=0&tags=story&hitsPerPage=1000';
+
+interface HNSearchResult {
+  isLoading: boolean;
+  error: Error | null;
+  items: Array<HNItem>;
+}
+
+const useHNSearch = (): HNSearchResult => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+  const [items, setItems] = useState<Array<HNItem>>([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setError(null);
+    fetch(DATA_URL)
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          console.log('error...');
+          throw Error('Error fetching the data!!!');
+        }
+      })
+      .then((items: { hits: [] }) => {
+        setIsLoading(false);
+        setItems(items.hits.map(mapFromJSON));
+      })
+      .catch(error => {
+        setIsLoading(false);
+        setError(error);
+      });
+  }, []);
+
+  return { isLoading, error, items };
+};
+
+export default useHNSearch;
