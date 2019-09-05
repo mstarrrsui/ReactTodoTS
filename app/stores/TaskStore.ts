c9bb1b0bd6250a9b38cf860ddc5b8525947f0c4d
Mike Starr <mstarr@rsui.com>
2019-09-05 08:53:35 -0400

    updates

--- app/stores/TaskStore.ts
+++ app/stores/TaskStore.ts
@@ -1,5 +1,6 @@
 import shortid from 'shortid';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { useState, useEffect } from 'react';
 
 interface Task {
   id: string;
@@ -26,6 +27,33 @@ class TaskStore {
     this._tasks = this._tasks.filter(i => !i.completed);
     this.tasks.next(this._tasks);
   }
+
+  loadFromLocalStorage(): void {
+    console.log('Loading taskList from localStorage...');
+    const STORAGE_KEY = 'todolist';
+    const data = localStorage.getItem(STORAGE_KEY);
+    // TODO - make this robust by checking shape of data or catching error and clearing old
+    if (data) {
+      console.log('Loaded taskList');
+      this._tasks = JSON.parse(data);
+    } else {
+      console.log('No data for taskList found in localStorage.  Init with defaults');
+      this._tasks = TaskListModel.create({ todoList: [], isLoading: false });
+    }
+  }
+}
+
+export function useObservable<T>(observable: Observable<T>, initialValue: T): T {
+  const [value, setValue] = useState(initialValue);
+
+  useEffect(() => {
+    const subscription = observable.subscribe(newValue => {
+      setValue(newValue);
+    });
+    return () => subscription.unsubscribe();
+  }, [observable]);
+
+  return value;
 }
 
 export const taskStore = new TaskStore();
