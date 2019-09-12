7c712b94ff89ca51485f372904c278ebea0fb35b
Mike Starr <mstarr@rsui.com>
2019-09-12 16:56:19 -0400

    cleanup

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -2,18 +2,29 @@ import React, { useEffect } from 'react';
 import Spinner from './Spinner';
 import TodoForm from './TodoForm';
 import TodoItems from './TodoItems';
+import log from 'loglevel';
+
 import { todoListState } from '../stores/TodoListState';
 import { useObservable } from '../util/useObservable';
 
 const TodoList: React.SFC = () => {
   const isLoading = useObservable(todoListState.isLoading, true);
+  const tasks = useObservable(todoListState.tasks, []);
 
   useEffect(() => {
     todoListState.loadFromLocalStorage();
   }, []);
 
+  function handleClick(): void {
+    log.debug('button pressed');
+    log.debug(JSON.stringify(tasks));
+    tasks.splice(0, 1);
+    log.debug(JSON.stringify(tasks));
+  }
+
   return (
     <div className="container todolist">
+      <button onClick={handleClick}>Press Me</button>
       <TodoForm />
       {isLoading ? <Spinner /> : <TodoItems />}
     </div>
