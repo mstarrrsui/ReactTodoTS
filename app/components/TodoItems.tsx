f9ee25d09554f2e6c68ecf4e125d87895bc2aefc
Mike Starr <mstarr@rsui.com>
2019-09-20 14:12:18 -0400

    cleanup

--- app/components/TodoItems.tsx
+++ app/components/TodoItems.tsx
@@ -1,45 +1,19 @@
 import * as React from 'react';
-import { useEffect } from 'react';
 import TodoItem from './TodoItem';
-import log from 'loglevel';
 
 import { todoListState } from '../stores/TodoListState';
 import { useObservable } from '../util/useObservable';
-import Spinner from './Spinner';
 
 const TodoItems: React.SFC = () => {
   const tasks = useObservable(todoListState.tasks, []);
-  const isLoading = useObservable(todoListState.isLoading, true);
-  //const isLoading = true;
 
-  useEffect(() => {
-    function loadData(): void {
-      log.debug('TodoList - Loading tasks');
-      todoListState.loadFromLocalStorage();
-    }
-
-    loadData();
-
-    return function cleanup() {
-      log.debug('TodoList Will Unmount');
-    };
-  }, []);
-
-  if (isLoading) {
-    return (
-      <div className="container todoitems">
-        <Spinner />
-      </div>
-    );
-  } else {
-    return (
-      <div className="container todoitems">
-        {tasks.map(item => (
-          <TodoItem item={item} key={item.id} />
-        ))}
-      </div>
-    );
-  }
+  return (
+    <div className="container todoitems">
+      {tasks.map(item => (
+        <TodoItem item={item} key={item.id} />
+      ))}
+    </div>
+  );
 };
 
 export default TodoItems;
--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -2,15 +2,32 @@ import React, { useEffect } from 'react';
 import Spinner from './Spinner';
 import TodoForm from './TodoForm';
 import TodoItems from './TodoItems';
+import log from 'loglevel';
 
 import { todoListState } from '../stores/TodoListState';
 import { useObservable } from '../util/useObservable';
 
 const TodoList: React.SFC = () => {
+  useObservable(todoListState.tasks, []);
+  const isLoading = useObservable(todoListState.isLoading, true);
+
+  useEffect(() => {
+    function loadData(): void {
+      log.debug('TodoList - Loading tasks');
+      todoListState.loadFromLocalStorage();
+    }
+
+    loadData();
+
+    return function cleanup() {
+      log.debug('TodoList Will Unmount');
+    };
+  }, []);
+
   return (
     <div className="container todolist">
       <TodoForm />
-      <TodoItems />
+      {isLoading ? <Spinner /> : <TodoItems />}
     </div>
   );
 };
