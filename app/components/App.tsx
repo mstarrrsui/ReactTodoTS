249026be7bec2b5e2cbd9cf846deb4d61eed4ae5
Mike Starr <mstarr@rsui.com>
2019-10-03 20:32:44 -0400

    cleanup  router in App

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -34,11 +34,7 @@ export default class App extends React.Component {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/todo" component={TodoList} />
-            <Route
-              exact
-              path="/search"
-              render={props => <SearchExample {...props} padTop="120px" />}
-            />
+            <Route exact path="/search" component={SearchExample} />
             <Route exact path="/hnsearch" component={HNSearch} />
             <Route exact path="/map" component={LocationFinder} />
             <Route render={returnNotFound} />
